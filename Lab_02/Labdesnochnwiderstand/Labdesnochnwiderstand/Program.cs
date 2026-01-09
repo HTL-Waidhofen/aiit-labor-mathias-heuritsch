94ba2405cc642be3993b08d5a07621c7c54ae6e2
mathias-heuritsch <mathias.heuritsch@htlwy.at>
2026-01-09 11:58:29 +0100

    Add files via upload

---
+++ Lab_02/Labdesnochnwiderstand/Labdesnochnwiderstand/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labdesnochnwiderstand
+{
+
+    class Resistor //Klasse Bauplan
+    {
+        //Atribute/Membervariablen
+        private string label; //Bezeichnung z.B. R1
+        private double value; //Widerstandswert
+        private double toleranz;//zb 1 wie 1%
+        private double maxPower;//zb. 10 fuer 10 Watt
+
+        //Methoden
+        //Konstruktor
+        public Resistor(string label, double value, double toleranz, double maxPower)
+        {
+            this.value = value;
+            this.label = label;
+            this.toleranz = toleranz;
+            this.maxPower = maxPower;
+        }
+        //zugriffsmethoden: Getter und Setter
+        public double GetValue()
+        { return value; }
+
+        public double calculatecurrent(double voltage)
+        {
+            double current = voltage / value;
+            return current;
+        }
+        public double calculatevoltage(double current)
+        {
+            double voltage = current * value;
+            return voltage;
+        }
+        public Resistor Seriel (Resistor r)
+        {
+            string newLabel = "Rges";
+            double newVal = r.value + this.value;
+
+            Resistor ges = new Resistor(newLabel, newVal, toleranz, maxPower);
+            
+
+
+            return ges;
+        }
+
+        public Resistor paralel(Resistor r)
+        {
+            string newLabel = "Rges";
+            double newVal = 1/(1/r.value + 1/this.value);
+
+            Resistor ges = new Resistor(newLabel, newVal, toleranz, maxPower);
+
+
+
+            return ges;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string eingabe = Console.ReadLine();
+            string[] geteilt = eingabe.Split(new string[] { "ohm" }, StringSplitOptions.None);
+
+            //Objekte sind Instanzen der Klasse
+            Resistor r1 = new Resistor("R1",100,5,10);
+            Resistor r2 = new Resistor("R2",200,1,20);
+            int voltage = 24;
+            double erg = r1.calculatecurrent(voltage);
+
+            Resistor Rges = r1.Seriel(r2);
+
+        }
+    }
+}
---
+++ Lab_02/Stromstärkeberechnen/Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Geben sie den Wert für den Widerstand an");
+string eingabe = Console.ReadLine();
+int positionzahl=0;
+int positionhochzahl=0;
+bool falscheAngabe;
+
+
+
+
+for(int i = 0;i<eingabe.Length;i++)
+{
+
+    if(eingabe[i] =='k' || eingabe[i] == 'o' || eingabe[i] == 'M' )
+    {
+        positionzahl = i - 1;
+        positionhochzahl = i;
+    }
+    
+}
+
+int zahlStr = int.Parse(eingabe.Substring(0, positionzahl));
+if(eingabe[positionhochzahl]=='k')
+{
+    zahlStr = zahlStr * (10 ^ 3);
+    Console.WriteLine("Widerstand={0}", zahlStr);
+}
+else if(eingabe[positionhochzahl] == 'M')
+{
+    zahlStr = zahlStr * (10 ^ 6);
+    Console.WriteLine("Widerstand={0}", zahlStr);
+}
+else if(eingabe[positionhochzahl] == 'o')
+{
+Console.WriteLine("Widerstand={0}",zahlStr);
+}
+else
+{ Console.WriteLine("Falsche Angabe");
+falscheAngabe = true;
+}
+int Widerstand = zahlStr;
+
+Console.WriteLine("Geben sie den Wert für die Spannung an");
+eingabe = Console.ReadLine();
+positionzahl = 0;
+positionhochzahl = 0;
+
+
+
+
+for (int i = 0; i < eingabe.Length; i++)
+{
+
+    if (eingabe[i] == 'k' || eingabe[i] == 'o' || eingabe[i] == 'm')
+    {
+        positionzahl = i - 1;
+        positionhochzahl = i;
+    }
+
+}
+
+zahlStr = int.Parse(eingabe.Substring(0, positionzahl));
+if (eingabe[positionhochzahl] == 'k')
+{
+    zahlStr = zahlStr * (10 ^ 3);
+    Console.WriteLine("Widerstand={0}", zahlStr);
+}
+else if (eingabe[positionhochzahl] == 'm')
+{
+    zahlStr = zahlStr * (10 ^ -3);
+    Console.WriteLine("Widerstand={0}", zahlStr);
+}
+else if (eingabe[positionhochzahl] == 'o')
+{
+    Console.WriteLine("Widerstand={0}", zahlStr);
+}
+else
+{ Console.WriteLine("Falsche Angabe"); falscheAngabe = true; }
+
+
+
+    Console.ReadKey();
